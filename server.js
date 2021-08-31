3dd4810a8c8ca9f83d7998e6562dd5da41aada50
Clint Edwards <cedwards1080@gmail.com>
2021-08-31 11:24:21 -0600

    adding template literal for port variable

--- server.js
+++ server.js
@@ -9,4 +9,4 @@ app.get('/', (req, res) => {
 
 const port = process.env.PORT || 4545
 
-app.listen(port, () => (console.log('Running on 4545 Captain')))
+app.listen(port, () => (console.log(`Running on ${port} Captain`)))
