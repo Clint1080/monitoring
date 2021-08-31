8bf7ce55aad9099d2e7e53015ec05befa8b11ceb
Clint Edwards <cedwards1080@gmail.com>
2021-08-31 12:04:54 -0600

    Added log and error handler also add student api

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const { resolveSoa } = require('dns')
 const express = require('express')
 const path = require('path')
 const Rollbar = require('rollbar')
@@ -8,11 +9,24 @@ const rollbar = new Rollbar({
     captureUnhandledRejections: true
 })
 
+const students = []
 const app = express()
+
+app.use(rollbar.errorHandler)
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './public/index.html'))
     rollbar.info('html file served successfully')
+})
+
+app.post('/api/student', (req, res) => {
+    const {name} = req.body
+    name = name.trim()
+
+    students.push(name)
+
+    rollbar.log('Student entered successfully', {author: 'Clint', type: 'manual entry'})
+    res.status(200).send(students)
 })
 
 
